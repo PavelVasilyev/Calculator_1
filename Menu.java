03c582d9d7f7027037d1dda100f0fa112847e974
PavelVasilyev <xdx5@yandex.ru>
2018-04-23 22:49:24 +0300

    1

--- Menu.java
+++ Menu.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Menu {
 
     public static void main(String[] args) {
-        System.out.println("1 - Массив\n2 - Калькулятор ");
+        System.out.println("1 - Массив\n2 - Калькулятор\n3 - Конструктор сладкого ");
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
 
@@ -14,6 +14,8 @@ public class Menu {
             case 2:
                 new Calculator();
                 break;
+            case 3:
+                new NYPresent();
         }
     }
 
